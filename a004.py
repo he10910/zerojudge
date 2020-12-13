d57bc552196df67f3d457a8fa4e53f950d353c1f
he10910 <>
2020-12-13 11:58:35 +0800

    a004

---
+++ a004.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        s = int(input())
+    except EOFError:
+        break
+
+    if s % 4 == 0 and s % 100 != 0 or s % 400 == 0:
+        print("閏年")
+    else:
+        print("平年")
